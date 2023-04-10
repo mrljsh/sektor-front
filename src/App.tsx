2b00638de6971ecbe2c02bde4a5b8ad7df42413e
Mrljsh <dragan.mrljes@vsar.edu.rs>
2023-04-10 12:23:17 +0200

    feat: adds navbar and dummy links and styles it

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import styled from 'styled-components'
 
 function App() {
   return (
     <AppContainer>
-      <Header><img src="/sektor44-logo.png" alt="Sektor44 logo" /></Header>
+      <Header>
+        <img src="/sektor44-logo.png" alt="Sektor44 logo" />
+        <Nav>
+          <ul>
+            <li>
+              <a href="#">POCETNA</a>
+            </li>
+            <li>
+              <a href="#">ODJAVI SE</a>
+            </li>
+          </ul>
+        </Nav>
+        </Header>
     </AppContainer>
   )
 }
@@ -17,10 +30,33 @@ const Header = styled.header`
   grid-area: navbar;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   background-color: fff;
   height: 64px;
   box-shadow: 0px 1px 2px 0px rgba(0,0,0,0.05);
   padding: 10px 40px;
+`
+
+const Nav = styled.nav`
+  & li {
+    display: inline-block;
+    margin: 5px 5px 5px 0;
+  }
+
+  & a {
+    padding: 10px 20px;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 12px;
+    font-weight: 500;
+    line-height: 1;
+    letter-spacing: 0.05em;
+    text-transform: uppercase;
+    transition: opacity 300ms ease;
+  }
+
+  & a:hover {
+    opacity: 0.6;
+  }
 `
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import App from './App'
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;800;900&family=Quicksand:wght@300;400;500;600;700&display=swap');
+
   *,
   *::before,
   *::after{
