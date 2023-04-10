1a1af441ddc2be184ef1c47e7d98f649a4121bc6
Mrljsh <dragan.mrljes@vsar.edu.rs>
2023-04-10 11:48:09 +0200

    chore: installs styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,21 @@
-import { useState } from 'react'
+import styled from 'styled-components'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <AppContainer>
+      <Header><h1>sektor44</h1></Header>
+    </AppContainer>
   )
 }
+
+const AppContainer = styled.div`
+  display: grid;
+  grid-template-areas: "navbar navbar navbar" "sidebar main main"  "sidebar main main";
+`
+
+const Header = styled.header`
+  grid-area: navbar;
+  background-color: yellow;
+`
 
 export default App
