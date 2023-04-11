d6a356990da83fc4179c9a0254851cf5a0023744
Mrljsh <dragan.mrljes@vsar.edu.rs>
2023-04-11 11:13:33 +0200

    feat: adds and styles members component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import Members from "./components/Members";
 
 function App() {
   return (
     <AppContainer>
       <Header />
       <Sidebar />
+      <Members />
     </AppContainer>
   );
 }
@@ -15,6 +17,7 @@ const AppContainer = styled.div`
   display: grid;
   grid-template-areas: "navbar navbar navbar" "sidebar main main" "sidebar main main";
   grid-template-rows: 64px auto auto;
+  grid-template-columns: 300px auto auto;
   height: 100vh;
 `;
 
---
+++ src/components/Members.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Members = () => {
+  return <MembersContainer>Members</MembersContainer>;
+};
+
+const MembersContainer = styled.main`
+  padding: 20px;
+  grid-area: main;
+`;
+
+export default Members;
